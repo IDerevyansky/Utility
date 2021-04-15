409616a9b1514ed0c29a9bbc3a48820faf6564b6
IDerevyansky <IDerevyansky@yandex.ru>
2021-04-15 03:49:37 +0300

    -

--- merge_images/index.php
+++ merge_images/index.php
@@ -1,36 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Аватарки</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
 
-function merge($thumb, $frame){
+        <form action="/merge" method="post" enctype="multipart/form-data">
+          
+            <input type="file" name="photo">
+            <button name="btn" type="submit">Далее</button>
 
-    $frame = imagecreatefrompng($frame);
-    $thumb = imagecreatefromjpeg($thumb);
-    $width = imagesx( $frame );
-    $height = imagesy( $frame );
+        </form>
 
-    $img=imagecreatetruecolor( $width, $height );
-
-    imagealphablending($img, true);
-
-    $transparent = imagecolorallocatealpha( $img, 0, 0, 0, 127 );
-    imagefill( $img, 0, 0, $transparent );
-    imagecopyresampled($img,$thumb,0,0,0,0, 200, 200, imagesx( $thumb ), imagesy( $thumb ) );
-    imagecopyresampled($img,$frame,0,0,0,0, $width,$height,$width,$height);
-    imagealphablending($img, false);
-    imagesavealpha($img,true);
-
-    //-------в последствии заменить на return-------
-    return $img;
-    //-------в последствии заменить на return-------
-
-    imagedestroy($img);
-    exit;
-}
-
-$thumb = './test_photo.jpg';
-$frame = './Bast_Sales.png';
-
-header('Content-type: image/png');
-imagepng( merge($thumb, $frame), NULL, 9 );
-
-
-?>
+    </body>
+</html>
---
+++ merge_images/merge/index.php
@@ -0,0 +1,52 @@
+<?php
+
+
+function merge($thumb, $frame){
+
+    $frame = imagecreatefrompng($frame);
+    
+    switch ( $_FILES['photo']['type'] ) {
+        case 'image/jpeg':
+            $thumb = imagecreatefromjpeg($thumb);
+            break;
+        case 'image/png':
+            $thumb = imagecreatefrompng($thumb);
+            break;
+        case 'image/gif':
+            $thumb = imagecreatefromgif($thumb);
+            break;   
+    }
+
+    $width = imagesx( $frame );
+    $height = imagesy( $frame );
+
+    $img=imagecreatetruecolor( $width, $height );
+
+    imagealphablending($img, true);
+
+    $transparent = imagecolorallocatealpha( $img, 0, 0, 0, 127 );
+    imagefill( $img, 0, 0, $transparent );
+    imagecopyresampled($img,$thumb,0,0,0,0, 200, 200, imagesx( $thumb ), imagesy( $thumb ) );
+    imagecopyresampled($img,$frame,0,0,0,0, $width,$height,$width,$height);
+    imagealphablending($img, false);
+    imagesavealpha($img,true);
+
+
+    return $img;
+
+    imagedestroy($img);
+    // unlink( "img/".$_FILES['photo']['name'] );
+    exit;
+}
+
+$file = $_FILES['photo']['name'];
+$target = "img/".$file;
+move_uploaded_file($_FILES['photo']['tmp_name'], $target);
+$thumb = $target;
+$frame = '../Bast_Sales.png';
+// echo "img/".$_FILES['photo']['name'];
+
+header("Content-type: getimagesize($target)");
+imagepng( merge($thumb, $frame), NULL, 9 );
+
+unlink( "img/".$_FILES['photo']['name'] );
