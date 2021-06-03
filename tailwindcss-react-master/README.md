fb07050df0d2ffd29e1c326898197141378bbe0b
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-06-03 18:30:43 +0300

    -

---
+++ tailwindcss-react-master/README.md
@@ -0,0 +1,19 @@
+Demo: https://thefubon.github.io/tailwindcss-react/dist/
+
+Install the required dependencies with `npm`:
+
+```sh
+npm install
+```
+
+Then start the dev server:
+
+```sh
+npm run dev
+```
+
+Or build for production to remove unused styles:
+
+```sh
+npm run build
+```
---
+++ tailwindcss-react-master/src/data/popularDestinations.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    city: "Moscow",
+    averagePrice: 620,
+    propertyCount: 196,
+    imageUrl: "/img/moscow.jpg",
+    imageAlt: "Moscow never sleap",
+  },
+  {
+    city: "Malibu",
+    averagePrice: 215,
+    propertyCount: 43,
+    imageUrl: "/img/malibu.jpg",
+    imageAlt: "Cliff in Malibu",
+  },
+  {
+    city: "Chicago",
+    averagePrice: 130,
+    propertyCount: 115,
+    imageUrl: "/img/chicago.jpg",
+    imageAlt: "Chicago skyline",
+  },
+  {
+    city: "Seattle",
+    averagePrice: 135,
+    propertyCount: 63,
+    imageUrl: "/img/seattle.jpg",
+    imageAlt: "Seattle skyline",
+  },
+  {
+    city: "Colorado",
+    averagePrice: 85,
+    propertyCount: 47,
+    imageUrl: "/img/colorado.jpg",
+    imageAlt: "Lake in Colorado",
+  },
+  {
+    city: "Miami",
+    averagePrice: 115,
+    propertyCount: 86,
+    imageUrl: "/img/miami.jpg",
+    imageAlt: "Beach in Miami",
+  },
+  {
+    city: "Зажопинк",
+    averagePrice: 1242,
+    propertyCount: 345,
+    imageUrl: "/img/miami.jpg",
+    imageAlt: "Beach in Miami",
+  }
+
+];
